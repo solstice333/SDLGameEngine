900613f471dccfff9999f0121d6ad352e0f218ae
knavero <knavero@gmail.com>
2013-09-10 22:18:59 -0700

    update: documented the main function to serve as a better example to newer developers who are using or adding onto the API

--- src/main/Test.cpp
+++ src/main/Test.cpp
@@ -15,6 +15,8 @@
 #include "PlayerFigure.h"
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
+#include "SDL/SDL_ttf.h"
+#include "SDL/SDL_mixer.h"
 
 using namespace std;
 
@@ -47,152 +49,121 @@ const string TTF_PATH = "fonts/lazy.ttf";
 const int FONT_SIZE = 28;
 const Surface::Color FONT_COLOR = Surface::BLACK;
 
-const bool FOO = true; //change this from true or false to choose between a stick figure
-//or dot ("FOO = true" is the stick figure)
-
 const Figure::Gravity gravEnDis = Figure::GRAVITY_ENABLED;
 //const Figure::Gravity gravEnDis = Figure::GRAVITY_DISABLED;
 
-const bool TEST_GRAPHICS = true;
-const bool TEST_STRING_INPUT = false;
-
 /*
- * TODO
- * Rebel - remove an object from collision map
- * and thus the scene
- *
- * Kevin - not necessary anymore since the Figures are set to INACTIVE? No need
- * to completely free memory from Figures that aren't shown anymore. Not until the
- * end of the level anyways. Any thoughts on this?
- */
-/*
- void removeIndex(vector<Figure*>& vec, int i) {
- //also free the memory assoicated with the pointer too
- vec.erase(vec.begin() + i);
- }
- */
-
-/*Description: This tests the scrolling, collision detection, static figures within the level,
- *and animation. User can switch const bool FOO to true/false and comment/uncomment const
- *Figure::Gravity gravEnDis for Enabling and Disabling gravity
+ * Description: This client simply tests the API and its capabilities of creating a
+ * simple 2d scrolling platform game. This will serve as an example for now.
  */
 int main(int argc, char* argv[]) {
-   if (TEST_GRAPHICS) {
-      SDL_Surface* screen = init(SCREEN_WIDTH, SCREEN_HEIGHT, "SDL Scrolling");
-      vector<Figure*> collisions;
+   //initialize screen, video mode, SDL, ttf, audio, etc.
+   SDL_Surface* screen = init(SCREEN_WIDTH, SCREEN_HEIGHT, "SDL Scrolling");
 
-      Surface bgnd("images/bgnd.jpg");
-      Surface dot("images/dot.png", Surface::CYAN);
-      Surface foo("images/Cyan_Final.png", Surface::BLACK);
-      Surface rect("images/rectangle.png");
-      Surface coin("images/coin.png", Surface::CYAN);
+   //initialize all images
+   Surface bgnd("images/bgnd.jpg");
+   Surface dot("images/dot.png", Surface::CYAN);
+   Surface foo("images/Cyan_Final.png", Surface::BLACK);
+   Surface rect("images/rectangle.png");
+   Surface coin("images/coin.png", Surface::CYAN);
 
-      Surface red("images/red.bmp", Surface::CYAN);
-      Surface blue("images/blue.bmp", Surface::CYAN);
-      Surface green("images/green.bmp", Surface::CYAN);
-      Surface shimmer("images/shimmer.bmp", Surface::CYAN);
+   Surface red("images/red.bmp", Surface::CYAN);
+   Surface blue("images/blue.bmp", Surface::CYAN);
+   Surface green("images/green.bmp", Surface::CYAN);
+   Surface shimmer("images/shimmer.bmp", Surface::CYAN);
 
-      RectBoundaryFigure rf1(300, 525, rect, screen, LEVEL_WIDTH, LEVEL_HEIGHT,
-            OTHER_NUMCLIPS);
-      RectBoundaryFigure rf2(500, 125, rect, screen, LEVEL_WIDTH, LEVEL_HEIGHT,
-            OTHER_NUMCLIPS);
-      CircBoundaryFigure cf1(700, 525, dot, screen, LEVEL_WIDTH, LEVEL_HEIGHT,
-            OTHER_NUMCLIPS);
-      CircBoundaryFigure cf2(900, 350, dot, screen, LEVEL_WIDTH, LEVEL_HEIGHT,
-            OTHER_NUMCLIPS);
+   //convert images to Figures
+   RectBoundaryFigure rf1(300, 525, rect, screen, LEVEL_WIDTH, LEVEL_HEIGHT,
+         OTHER_NUMCLIPS);
+   RectBoundaryFigure rf2(500, 125, rect, screen, LEVEL_WIDTH, LEVEL_HEIGHT,
+         OTHER_NUMCLIPS);
+   CircBoundaryFigure cf1(700, 525, dot, screen, LEVEL_WIDTH, LEVEL_HEIGHT,
+         OTHER_NUMCLIPS);
+   CircBoundaryFigure cf2(900, 350, dot, screen, LEVEL_WIDTH, LEVEL_HEIGHT,
+         OTHER_NUMCLIPS);
 
-      TempFigure coin1(600, 325, coin, screen, LEVEL_WIDTH, LEVEL_HEIGHT);
+   TempFigure coin1(600, 325, coin, screen, LEVEL_WIDTH, LEVEL_HEIGHT);
 
-      PlayerFigure rf;
-      CircFigure cf;
+   PlayerFigure rf(100, LEVEL_HEIGHT - foo.getSDL_Surface()->h / 2, foo, screen,
+         FS, G, FJS, FNC, LEVEL_WIDTH, LEVEL_HEIGHT, &red, &green, &blue,
+         &shimmer);
 
-      if (FOO) {
-         rf.setFigure(100, LEVEL_HEIGHT - foo.getSDL_Surface()->h / 2, foo,
-               screen, FS, G, FJS, FNC, LEVEL_WIDTH, LEVEL_HEIGHT, &red, &green,
-               &blue, &shimmer);
+   //Prepare music to be played within level
+   Music m("resources/tristam.mp3");
 
-         collisions.push_back(&rf);
+   //collision vector - contains all the Figures (pointers to Figures) that
+   //must be taken into account in regards to collision detection
+   vector<Figure*> collisions;
+
+   collisions.push_back(&rf);
+   collisions.push_back(&rf1);
+   collisions.push_back(&rf2);
+   collisions.push_back(&cf1);
+   collisions.push_back(&cf2);
+   collisions.push_back(&coin1);
+
+   //Prepare bool quit variable, SDL_Event event, and Timer timer. All of these
+   //variables will be used in the event loop
+   bool quit = false;
+   SDL_Event event;
+   Timer timer;
+   timer.start();
+
+   //Start music and set volume
+   if (Mix_PlayingMusic() == 0)
+      if (Mix_PlayMusic(m.getMix_Music(), -1) < 0)
+         throw SoundException();
+
+   Mix_VolumeMusic(32); //0 to 128
+
+   //Start event loop
+   while (!quit) {
+      if (SDL_PollEvent(&event)) {
+         if (event.type == SDL_QUIT)
+            quit = true;
+
+         //receive input for Player Figure
+         rf.handleInput(event);
       }
-      else {
-         cf.setFigure(100, 300, dot, screen, gravEnDis, LEVEL_WIDTH,
-               LEVEL_HEIGHT, true, CS, G, CJS, CNC);
 
-         collisions.push_back(&cf);
-      }
+      //move Player Figure to new position based on input given
+      rf.move(collisions, timer.getTicks());
 
-      //Test constructors
-      /*
-       RectFigure rf(100, 300, foo, screen, gravEnDis, true, FS, G, FJS, FNC,
-       LEVEL_WIDTH, LEVEL_HEIGHT, &red, &green, &blue, &shimmer);
-       CircFigure cf(100, 300, dot, screen, gravEnDis, true, CS, G, CJS, CNC,
-       LEVEL_WIDTH, LEVEL_HEIGHT, &red, &green, &blue, &shimmer);
-       */
-
-      bool quit = false;
-      SDL_Event event;
-      Timer timer;
-
-      collisions.push_back(&rf1);
-      collisions.push_back(&rf2);
-      collisions.push_back(&cf1);
-      collisions.push_back(&cf2);
-      collisions.push_back(&coin1);
-
-      Music m("resources/tristam.mp3");
-
+      //restart timer since movement is time-based and independent of framerate
       timer.start();
 
-      if (Mix_PlayingMusic() == 0)
-         if (Mix_PlayMusic(m.getMix_Music(), -1) < 0)
-            throw SoundException();
+      //blit background image to screen with respect to the camera following Player Figure
+      applySurface(0, 0, bgnd, screen, rf.getCameraClip());
 
-      Mix_VolumeMusic(32); //0 to 128
+      //show the Player Figure (argument is optional here since the relative camera is
+      //the Player Figure's camera
+      rf.show(rf.getCameraClip());
 
-      while (!quit) {
-         if (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT)
-               quit = true;
+      //show all other Figures with respect to the relative camera i.e. the Player
+      //Figure's camera
+      rf1.show(rf.getCameraClip());
+      rf2.show(rf.getCameraClip());
+      cf1.show(rf.getCameraClip());
+      cf2.show(rf.getCameraClip());
 
-            if (FOO)
-               rf.handleInput(event);
-            else
-               cf.handleInput(event);
-         }
+      //we need to move coin1 so that collision detection is not
+      //ignored (although actual movement of coin1 doesn't happen because no input
+      //is received). First argument is the collision vector. Second argument is the
+      //delta ticks from the timer which is irrelevant since coin1 does not actually
+      //move. It just disappears when collided with the Player Figure
+      coin1.move(collisions, 0);
 
-         if (FOO) {
-            rf.move(collisions, timer.getTicks());
-            timer.start();
+      //show coin1 based on the update caused by move()
+      coin1.show(rf.getCameraClip());
 
-            applySurface(0, 0, bgnd, screen, rf.getCameraClip());
-            rf.show(rf.getCameraClip());
-
-            rf1.show(rf.getCameraClip());
-            rf2.show(rf.getCameraClip());
-            cf1.show(rf.getCameraClip());
-            cf2.show(rf.getCameraClip());
-
-            coin1.move(collisions, 0);
-            coin1.show(rf.getCameraClip());
-         }
-         else {
-            cf.move(collisions, timer.getTicks());
-            timer.start();
-
-            applySurface(0, 0, bgnd, screen, cf.getCameraClip());
-            cf.show();
-
-            rf1.show(cf.getCameraClip());
-            rf2.show(cf.getCameraClip());
-            cf1.show(cf.getCameraClip());
-            cf2.show(cf.getCameraClip());
-         }
-
-         flip(screen);
-      }
-
-      Mix_HaltMusic();
+      //update the screen by swapping video buffers
+      flip(screen);
    }
 
+   //stop music
+   Mix_HaltMusic();
+
+   //quit out of SDL, close audio, ttf, etc.
    cleanUp();
 
    return 0;
